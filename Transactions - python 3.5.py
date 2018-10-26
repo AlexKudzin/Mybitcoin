66937c19f626c370278570fae6b7d19162c35b70
AlexKudzin <44494856+AlexKudzin@users.noreply.github.com>
2018-10-26 12:11:35 +0300

    Update Transactions - python 3.5.py

--- Transactions - python 3.5.py
+++ Transactions - python 3.5.py
@@ -1,4 +1,5 @@
 #p2pkh
+# Thanks for the amazing tutorials could you please do an updated on for python3? or add a lot of comments to this? 
 
 version = (1).to_bytes(4, byteorder="little", signed=False)
 
@@ -15,10 +16,10 @@ lock_time = (0).to_bytes(4, byteorder="little", signed=False)
 
 tx_out_value = (299000000).to_bytes(8, byteorder="little", signed=True)
 
-tx_out_pubScript = bytes.fromhex("76a91431726579c6ab17fe2f85e236309d4c0bcff2805588ac")      
-tx_out_pubScript_size = bytes.fromhex("19")
+tx_out_pubScript = bytes.fromhex("76a91431726579c6ab17fe2f85e236309d4c0bcff2805588ac") #what is this a hash of?      
+tx_out_pubScript_size = bytes.fromhex("19") #why?
 
-prv_tx_id = bytes.fromhex("192730e77f297a5b805fac2a833948c761e79c1d74929d170523d624d20aff193")
+prv_tx_id = bytes.fromhex("192730e77f297a5b805fac2a833948c761e79c1d74929d170523d624d20aff193") #what is this a hash of?
 
 reversed_prv_tx_id = bytearray(prv_tx_id)
 
@@ -26,16 +27,16 @@ reversed_prv_tx_id.reverse()
 
 index = (2).to_bytes(4, byteorder="little", signed=False)
 
-sigScript_raw = bytes.fromhex("76a91486501b046d9c67aa1e361cbf49cfc6482fd16d1b88ac")
+sigScript_raw = bytes.fromhex("76a91486501b046d9c67aa1e361cbf49cfc6482fd16d1b88ac") #same as tx_out_pubScript what is this a hash of?
 
-sigScript_raw_size = bytes.fromhex("19")
+sigScript_raw_size = bytes.fromhex("19") #same as tx_out_pubScript_size, why 19?
 
-sequence = bytes.fromhex("ffffffff")
+sequence = bytes.fromhex("ffffffff") #why?
 
 raw_tx = ( version
             + tx_in_count
             + reversed_prv_tx_id
-            + index
+            + indexf8afc01944a1126af36e98a19060000008a4
             + sigScript_raw_size
             + sigScript_raw
             + sequence
@@ -50,7 +51,7 @@ raw_tx = ( version
 
 print(raw_tx.hex())
 
-import hashlib
+import hashlibf8afc01944a1126af36e98a19060000008a4
 
 hash_1 = hashlib.sha256(raw_tx).digest()
 
@@ -99,7 +100,7 @@ REAL_TX = ( version
 print(REAL_TX.hex())
 
 
-hash_1 = hashlib.sha256(bytes.fromhex("0100000001e18e68f7fc653a9f3e249c887f03963bf5c8d10f8afc01944a1126af36e98a19060000008a4730440220113f91f8d10725b3a279cf4bd4de3d43c21d94210d22acde8f9e58c7240e884c022061b05a6ccdf8dbad5b34eb57ff6832eb2a7cb584b828bdd8ada6ec843438348d01410448d297e22dbd448f2a00501a6336c15b809df00228c527e4221338d0ce9999439e80f93a6b4c1e8281724967a62bcf44142d46dfc72d31f58b8304556f5f70aaffffffff01c060d211000000001976a91486501b046d9c67aa1e361cbf49cfc6482fd16d1b88ac00000000")).digest()
+hash_1 = hashlib.sha256(bytes.fromhex("0100000001e18e68f7fc653a9f3e249c887f03963bf5c8d10f8afc01944a1126af36e98a19060000008a4730440220113f91f8d10725b3a279cf4bd4de3d43c21d94210d22acde8f9e58c7240e884c022061b05a6ccdf8dbad5b34eb57ff6832eb2a7cb584b828bdd8ada6ec843438348d01410448d297e22dbd448f2a00501a6336c15b809df00228c527e4221338d0ce9999439e80f93a6b4c1e8281724967a62bcf44142d46dfc72d31f58b8304556f5f70aaffffffff01c060d211000000001976a91486501b046d9c67aa1e361cbf49cfc6482fd16d1b88ac00000000")).digest() #why this really long hash?
 
 hash_2 = hashlib.sha256(hash_1).digest()
 
